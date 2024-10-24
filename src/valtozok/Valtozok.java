199ab94f9578647ac9b0901fabc8c707703db6b6
Bori888 <ilyes.bori8@gmail.com>
2024-10-24 14:12:07 +0200

    kesz

---
+++ src/valtozok/Valtozok.java
@@ -0,0 +1,15 @@
+package valtozok;
+
+public class Valtozok {
+
+    public static void main(String[] args) {
+        String nev = "Bori";
+        
+        System.out.print("Szia ");
+        //System.out.print("Bori");
+        System.out.print(nev);
+        System.out.println("!");
+
+    }
+
+}
